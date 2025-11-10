5413c1cb9fa5f6431d8fb142097d5feea27db185
sebastian_rupprecht <sebastian.rupprecht@tim-solutions.de>
2025-11-10 15:45:34 +0100

    fix: customer detail card

--- ui/projects/shell/src/app/features/authorized/customer/detail/customer-detail.component.ts
+++ ui/projects/shell/src/app/features/authorized/customer/detail/customer-detail.component.ts
@@ -2,14 +2,13 @@ import { ChangeDetectionStrategy, Component, inject, signal } from "@angular/cor
 import { RouterLink } from "@angular/router";
 import { TranslatePipe } from "@ngx-translate/core";
 import { ButtonModule } from "primeng/button";
-import { CardModule } from "primeng/card";
 import { DividerModule } from "primeng/divider";
 import { CustomerStore } from "../../../../data-access/store/customer.store";
 import { injectParam } from "../../../../utils/signals/inject-param";
 
 @Component({
     selector: 'app-customer-detail',
-    imports: [CardModule, ButtonModule, DividerModule, TranslatePipe, RouterLink],
+    imports: [ButtonModule, DividerModule, TranslatePipe, RouterLink],
     templateUrl: 'customer-detail.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush,
     host: { class: 'flex p-4 w-full justify-center' },
